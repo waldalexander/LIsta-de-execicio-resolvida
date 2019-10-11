4cdde5ff6fa45fa8ca49eb676a426aa5a141d64e
waldalexander <53957223+waldalexander@users.noreply.github.com>
2019-10-11 20:57:06 -0300

    Create l6q4.c

---
+++ l6q4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main (void){
+
+char nome[50];
+
+    printf("digite um nome \n");
+        gets(nome);
+            if(nome[]){
+                printf("nome partido %s", nome);
+    }else{
+                printf("nome inteiro: ");
+    }
+
+}
