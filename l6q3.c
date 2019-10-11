deadb9dc4ebf0d65f6a916434c383513ea53333f
waldalexander <53957223+waldalexander@users.noreply.github.com>
2019-10-10 21:09:01 -0300

    Add files via upload

---
+++ l6q3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void){
+    char nome[100];
+        printf("digite um nome \n");
+            gets(nome);
+
+ if (nome[0] =='A') {
+        printf("%s");
+        }
+
+ }
+
+
+
+
