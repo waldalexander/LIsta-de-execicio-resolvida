9518ec7299c255e5ad0dd0b6660efd6ddc30cac6
waldalexander <53957223+waldalexander@users.noreply.github.com>
2019-10-10 21:15:41 -0300

    Update l6q3.c

--- l6q3.c
+++ l6q3.c
@@ -4,12 +4,13 @@ int main(void){
     char nome[100];
         printf("digite um nome \n");
             gets(nome);
+ if (nome[0] =='A' || nome[0] == 'a'){
+        printf("%s",nome);
+ }else{
+        printf("A palavra nao inicia com A.");
+}
 
- if (nome[0] =='A') {
-        printf("%s");
-        }
-
- }
+}
 
 
 
